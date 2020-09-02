72e9fc8f08822fa3076467924122b37a273a6811
eseyas.asmelash <ezaweld@gmail.com>
2020-09-02 15:24:45 +0200

    merge to master

---
+++ webapi2/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webapi2.Models;
+
+namespace webapi2.Controllers
+{
+    [Route("Api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+                Id =  1,
+                Name = "Austin",
+                Description = "English Car",
+                Price = 10000
+            },
+            new Product
+            {
+                Id =  2,
+                Name = "Audi",
+                Description = "german Car",
+                Price = 20000
+            },
+            new Product
+            {
+                Id =  3,
+                Name = "Fiat",
+                Description = "Italian Car",
+                Price = 13000
+            },
+             new Product
+            {
+                Id =  4,
+                Name = "Citroen",
+                Description = "French Car",
+                Price = 14000
+            },
+        };
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            return product;
+        }
+
+    }
+}
