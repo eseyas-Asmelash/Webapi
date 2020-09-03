271e621d8451a98abbcf6fa12388b2f1b79a8673
eseyas.asmelash <ezaweld@gmail.com>
2020-09-03 15:35:22 +0200

    blazor api

---
+++ BlazorWebApi/Entities/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebApi.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
